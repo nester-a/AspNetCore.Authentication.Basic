f49f6fd4f74395c65142291f01baa26616228139
Aleksey_N <nester.a.l@yandex.ru>
2023-11-13 22:28:01 +0300

    added extension methods for injection

---
+++ src/AspNetCore.Authentication.Basic/BasicExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System;
+
+namespace AspNetCore.Authentication.Basic
+{
+    public static class BasicExtensions
+    {
+        public static AuthenticationBuilder AddBasic(this AuthenticationBuilder builder)
+        {
+            return builder.AddBasic(BasicDefaults.AuthenticationScheme);
+        }
+
+        public static AuthenticationBuilder AddBasic(this AuthenticationBuilder builder, string authenticationScheme)
+        {
+            return builder.AddBasic(authenticationScheme, _ => { });
+        }
+
+        public static AuthenticationBuilder AddBasic(this AuthenticationBuilder builder, Action<BasicOptions> configure)
+        {
+            return builder.AddBasic(BasicDefaults.AuthenticationScheme, configure);
+        }
+
+        public static AuthenticationBuilder AddBasic(this AuthenticationBuilder builder, string authenticationScheme, Action<BasicOptions> configure)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if(authenticationScheme == null)
+            {
+                throw new ArgumentNullException(nameof(authenticationScheme));
+            }
+
+            if(configure == null)
+            {
+                throw new ArgumentNullException(nameof(configure));
+            }
+
+            return builder.AddScheme<BasicOptions, BasicHandler>(authenticationScheme, configure);
+        }
+    }
+}
